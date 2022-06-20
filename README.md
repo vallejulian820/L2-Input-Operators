268a0d2bb7c864a0ab3bdbbe3a150660f5702c32
Julian Valle <107879937+vallejulian820@users.noreply.github.com>
2022-06-20 19:05:06 +0000

    madlibs

---
+++ README.md
@@ -0,0 +1,16 @@
+## Input
+Input is the way the computer gets information from the user
+
+Note: Python initially treats input as a `string` type
+
+Ex.
+
+5 --> "5"
+Today is Monday --> "Today is Monday"
+
+### Formula:
+`variableName = input("PROMPT")`
+
+PROMT _ A question or statement that the computer ask the user
+
+Python stores the __input__ in the __variableName__
---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+userName = input("Enter your name: ")
+#print("Hey " + userName)
+
+userGrade = input("WHa grade are you in? ") 
+print("Hey " + userName + ", " + userGrade + " is a lame grade")
+
+userYear = int(input("What year were you born? "))
+userAge = 2022 - userYear
+print(str(userAge) + " is so young!")
+'''
+
+# Create your own Madlibs
+# Have at least 5 inputs
+verb1 = input("Please give me a verb: ")
+adjective1 = input("Please give me an adjective")
+noun1 = input("Please give me a plural noun")
+adjective2 = input("Please give me a adjective")
+verb2 = input("Please give me a verb ending in ing")
+verb3 = input("Please give me a verb")
+number = input("Please give me a number")
+adjective3 = input("Please give me an adjective")
+noun2 = input("Please give me a plural noun")
